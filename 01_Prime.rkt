07613c5c8e68a8d392dfe993e087d7a8dce6aa77
Rebecca <rebecca.kane@outlook.com>
2018-02-15 17:31:40 +0000

    Starting first problem

---
+++ 01_Prime.rkt
@@ -0,0 +1,13 @@
+#lang racket
+; Write, from scratch, a function in Racket that uses a brute-force algorithm that takes a single positive integer and return true if the number is a prime and false otherwise. Call the function decide-prime.
+
+; Prime number = number greater than 1 whose only factors are itself and 1. 1 is neither prime nor composite, 2 is prime.
+
+; Brute force - take every number greater than 1 and less than the given number and see if it divides into the given number evenly..?
+
+(define(decide-prime)
+  (display "Hello, World!")
+
+)
+
+(decide-prime)
