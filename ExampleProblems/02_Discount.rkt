c2e0a52b3e0fe1d982616e12710ed5ded4741421
Rebecca <rebecca.kane@outlook.com>
2018-02-20 10:07:30 +0000

    Tidied up

---
+++ ExampleProblems/02_Discount.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+; Define a procedure discount that takes two args (item price and discount), return new price.
+; Based on video lecture "Racket Discount Example"
+
+(define (discount orig disc) (- orig (* orig(/ disc 100.0) )))
+; Get the discount - divide discount by 100 to get amount in decimal (5% = 0.05), multiply by the original, take answer from the original.
+
+(discount 10 5)
+
+(discount 29.90 50)
