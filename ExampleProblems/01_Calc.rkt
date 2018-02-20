c5454a476a0702dcd33711604e0b332ed17490c7
Rebecca <rebecca.kane@outlook.com>
2018-02-20 09:45:25 +0000

    B working

--- ExampleProblems/01_Calc.rkt
+++ ExampleProblems/01_Calc.rkt
@@ -13,3 +13,5 @@
 (* 3 (+ 5 (/ 10 5)))
 
 ; b)
+(display "(2 + 3 + 4 + 5) = ")
+( + 2 3 4 5) ; only one operator needed for multiple operands - http://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Numerical-operations.html
