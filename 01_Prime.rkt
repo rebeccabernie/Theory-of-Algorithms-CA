4a8bde45b2848ddee106519be37deee13aba9d18
Rebecca <rebecca.kane@outlook.com>
2018-02-20 14:12:50 +0000

    Seems to be working

--- 01_Prime.rkt
+++ 01_Prime.rkt
@@ -5,18 +5,33 @@
 
 ; Brute force - take every number greater than 1 and less than the given number and see if it divides into the given number evenly..?
 
+(define (check p n)
+  (if (= p n) ; if the given number and its divisor are the same
+    (display "Prime") ; #t
+    (if (= (modulo p n) 0)
+    ; If given number and current iteration divide without remainder
+      (display "Not a prime") ; #f
+      (check p (+ n 1)) ; check again with next iteration
+    )
+  )
+)
+
 (define(decide-prime p)
-  (if (< p 2) (display "Not a prime") 
-      (if (= p 2) (display "prime")
-       (define x p)
-       (if (/ x p
-       ); end if p = 2
-   ); end if p = 1
+  (if (< p 2) 
+    (display "Not a prime") ; Any number less than 2 = not a prime
+    (check p 2) ; Check to see if prime
+  )
 ) 
 
-(display "1: ")
+(display "1:  ")
 (decide-prime 1)
-(display "\n2: ")
+(display "\n2:  ")
 (decide-prime 2)
-(display "\n-1: ")
-(decide-prime -1)
+(display "\n42: ")
+(decide-prime 42)
+(display "\n43: ")
+(decide-prime 43)
+(display "\n71: ")
+(decide-prime 71)
+(display "\n72: ")
+(decide-prime 72)
