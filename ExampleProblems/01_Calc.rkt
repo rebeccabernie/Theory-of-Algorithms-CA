9ecfb9a4dbb27886736d540a348e7b6b84821112
Rebecca <rebecca.kane@outlook.com>
2018-02-20 09:41:22 +0000

    Working on example problem sheet

---
+++ ExampleProblems/01_Calc.rkt
@@ -0,0 +1,15 @@
+#lang racket
+; Re-write the following expressions in Racket and evaluate them using a Racket interpreter/compiler.
+;(a) (3 × (5 + (10 ÷ 5)))
+;(b) (2 + 3 + 4 + 5)
+;(c) (1 + (5 + (2 + (10 ÷ 3))))
+;(d) (1 + (5 + (2 + (10 ÷ 3.0))))
+;(e) (3 + 5) × (10 ÷ 2)
+;(f) (3 + 5) × (10 ÷ 2) + (1 + (5 + (2 + (10 ÷ 3))))
+
+
+; a)
+(display "(3 × (5 + (10 ÷ 5))) = ")
+(* 3 (+ 5 (/ 10 5)))
+
+; b)
