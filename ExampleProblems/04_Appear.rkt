d8e07e9cbc287caae9409f3e6131b916a2f16eca
Rebecca <rebecca.kane@outlook.com>
2018-02-20 12:22:51 +0000

    Finished problem

--- ExampleProblems/04_Appear.rkt
+++ ExampleProblems/04_Appear.rkt
@@ -1,10 +1,15 @@
 #lang racket
 ; Write a function called appearances that returns the number of times its first argument appears as a member of its second argument.
 
+; car = first el in list, cdr = rest of list. https://www.gnu.org/software/emacs/manual/html_node/eintr/car-_0026-cdr.html#car-_0026-cdr
+; cons = constructs lists - (cons 'pine '(fir oak maple)) prints (pine fir oak maple). https://www.gnu.org/software/emacs/manual/html_node/eintr/cons.html#cons
+
 (define (appearances i l)
   (if (null? l) ; Check if list is null first, if so first argument appears 0 times
     0
-    (display "not null")
+    (if (equal? i (car l)) ; if i = first el of list
+      (+ 1 (appearances i (cdr l))) ; true = + 1, call function again with remaining els in list
+      (appearances i (cdr l))) ; false = just call function again
     ))
 
-(appearances 1 (list 1 1 2 3 4))
+(appearances 3 (list 1 1 2 3 4 3 43))
