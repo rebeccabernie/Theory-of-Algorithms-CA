1fdf6f3491f856d2b2cba0f82c3cff6e888fc5a6
Rebecca <rebecca.kane@outlook.com>
2018-03-05 13:21:42 +0000

    Both functions working fin

--- 03_Cycle.rkt
+++ 03_Cycle.rkt
@@ -28,7 +28,7 @@
          (append (flatten-list (car l)) (flatten-list (cdr l))))
         (else (list l))))
 
-;(lcycle (list 1 2 3 4 5))
+(lcycle (list 1 2 3 4 5))
 (rcycle (list 1 2 3 4 5))
 ;(rcycle (list 5 6 7 8 9 10 11)) ; Testing it works with longer lists (it does)
 
