cbe23e483caebd4144a3e8acdb5d4f6c03aeab70
Rebecca <rebecca.kane@outlook.com>
2018-03-13 10:34:07 +0000

    Trying cond approach, still getting errors

--- 04_SublistSum.rkt
+++ 04_SublistSum.rkt
@@ -5,22 +5,22 @@
 
 (define (sublsum l1)
     (combinations l1)
-    (null? l1)
+    (if (null? l1)
         '()
         (sum (cdr (combinations l1))) ; First el will always be the empty set..?
+    )
 )
 
 (define (sum comb)
-  (if (null? comb) ; if the current combination is null
-    '() ; output 0 - sum of nothing is nothing
-    (if (null? (cdr (car comb)))
-        (sum (cdr comb))
-        (if (= 0 (+ (car (car comb)) (sum (cdr (car comb))))) ; else add recursively sum the cdr each time, add to car
-            (car comb) ; if the sum of the combination is 0, output the combination
-            (sum(cdr comb)) ; else not 0, pass back in
-        )
-    )
-  )
+    (cond   ((null? comb) '())) ; if the comb is null output empty
+            ((null? (car comb)) (sum(cdr comb))) ; if the first el of the comb is null pass the cdr back in
+            ((null? (cdr (car comb))) (sum(cdr comb))) ; if there's only one digit in the element pass the cdr back in
+            
+                (if (= 0 (+ (car (car comb)) (sum (cdr (car comb))))) ; else add recursively sum the cdr each time, add to car
+                    (car comb) ; if the sum of the combination is 0, output the combination
+                    (sum(cdr comb)) ; else not 0, pass back in
+                )
+            
 )
 
 (sublsum (list 1 2 3 4 -5))
