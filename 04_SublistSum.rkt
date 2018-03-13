69e943650a98c5b2201ccf4845fba17e310801ff
Rebecca <rebecca.kane@outlook.com>
2018-03-13 10:00:11 +0000

    Sums first combination, working on looping through combinations

--- 04_SublistSum.rkt
+++ 04_SublistSum.rkt
@@ -4,12 +4,20 @@
 ;               (sublsum (list 1 2 3 4 5))   =>     '()
 
 (define (sublsum l1)
-    ;(calculate (car (combinations l1)))
-(car (combinations l1))
+    (combinations l1)
+    (null? l1)
+        '()
+        (sum (cdr (combinations l1))) ; First el will always be the empty set..?
 )
 
-(define (calculate sl)
-    (+ (car sl) (calculate (cdr sl)))
+(define (sum comb)
+  (if (null? comb) ; if the current combination is null
+    0 ; output 0 - sum of nothing is nothing
+    (if (= 0 (+ (car (car comb)) (sum (cdr (car comb))))) ; else add recursively sum the cdr each time, add to car
+        comb ; if the sum of the combination is 0, output the combination
+        "not 0" ; else not 0, do something
+    )
+  )
 )
 
 (sublsum (list 1 2 3 4 -5))
