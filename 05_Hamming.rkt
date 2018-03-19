bfe9328dd435f457f87cd6f128d9927cccbcb6bd
Rebecca <rebecca.kane@outlook.com>
2018-03-19 17:52:19 +0000

    Trying similar approach to 04_SublistSum

--- 05_Hamming.rkt
+++ 05_Hamming.rkt
@@ -2,7 +2,10 @@
 ; Write a function hamming-weight in Racket that takes a list l as input and returns the number of non-zero elements in it. For example:    (hamming-weight (list 1 0 1 0 1 1 1 0))  =>  5
 
 (define (hamming-weight l1)
-    l1
+    (if (= 0 (car l1))
+        (+ (car l1) (hamming-weight (cdr l1)))
+        (hamming-weight (cdr l1))
+    )
 )
 
 (hamming-weight (list 1 0 1 0 1 1 1 0))
