a3a6284d0f93bf7fb26156cae890b64cf3b6a2b4
Rebecca <rebecca.kane@outlook.com>
2018-03-20 15:25:41 +0000

    Renamed 05, starting 06

---
+++ 06_HammingD.rkt
@@ -0,0 +1,9 @@
+#lang racket
+; Write a function hamming-distance in Racket that takes two lists and returns the number of positions in which they diﬀer. Example: (hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0)) => 5 
+
+
+(define (hamming-distance l1 l2)
+    l1
+)
+
+(hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
