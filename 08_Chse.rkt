b2cd13c65b05b3989746066236390ed0f281aff1
Rebecca <rebecca.kane@outlook.com>
2018-03-20 19:19:49 +0000

    Problem finished... very similar to 07_Maj.rkt

--- 08_Chse.rkt
+++ 08_Chse.rkt
@@ -3,7 +3,15 @@
 ; It should return a list containing the elements of y in the positions where x is 1 and the elements of z otherwise.
 ; For example: > (chse (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))  =>  '(0 1 0 1 0 0 1 1) 
 
-(define (chse l1 l2 l3)
-    l1
+
+; In plain english... if el in x is 1, return el at same index in y, if x is 0, return el at same index in z
+; Very similar to 07_Maj.rkt
+(define (chse x y z)
+    (cond [(null? x) '()] ; If the list is null return an empty list - all lists of equal length so if one is empty, they all are
+        [(= (car x) 1)(cons (car y)(chse (cdr x)(cdr y)(cdr z)))] ; If car x is 1, cons the car y and pass the cdrs of each list back into chse 
+        [else (cons (car z)(chse (cdr x)(cdr y)(cdr z)))] ; Else (x must be 0), cons the car z and pass the cdrs of each list back to chse
+    )
 )
+
 (chse (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
+;(chse (list 0 1 0 1) (list 1 2 3 4) (list 5 6 7 8)) ; Testing to make sure
