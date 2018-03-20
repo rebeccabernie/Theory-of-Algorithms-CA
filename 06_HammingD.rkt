7df45dacd7bcb4c180d165cb7e7514a80a0aeb9a
Rebecca <rebecca.kane@outlook.com>
2018-03-20 15:32:31 +0000

    Trying recursively checking, not working yet

--- 06_HammingD.rkt
+++ 06_HammingD.rkt
@@ -3,7 +3,13 @@
 
 
 (define (hamming-distance l1 l2)
-    l1
+    (if (null? (cdr l1))
+        (+ (car l1) 0)
+        (if (= (car l1) (car l2))
+            (hamming-distance (cdr l1)(cdr l2))
+            (+ (car l1)(hamming-distance (cdr l1)(cdr l2)))
+        )
+    )
 )
 
 (hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
