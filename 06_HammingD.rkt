c2ec26af6dbb4234bf6f844d85346df8b6551b1e
Rebecca <rebecca.kane@outlook.com>
2018-03-22 10:21:33 +0000

    Tidied up, replaced if with cond

--- 06_HammingD.rkt
+++ 06_HammingD.rkt
@@ -2,16 +2,13 @@
 ; Write a function hamming-distance in Racket that takes two lists and returns the number of positions in which they diﬀer. Example: (hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0)) => 5 
 
 (define (hamming-distance l1 l2)
-    (if (null? l1) ; Checks if either list is null
-        0 ; If so do nothing..?
-        (if (null? l2)
-            0
-            (if (= (car l1) (car l2)) ; Otherwise if the two elements are equal...
-                (hamming-distance (cdr l1)(cdr l2)) ; Don't add to count, check remainder of list
-                (+ 1 (hamming-distance (cdr l1)(cdr l2))) ; Add to count, check remainder of list
-            )
-        )
+    (cond   
+        [(null? l1) (length l2)] ; Differs in (length l2) number of places
+        [(null? l2) (length l1)] ; Same as above, swapped
+        [(= (car l1) (car l2)) (hamming-distance (cdr l1)(cdr l2))] ; If cars the same, don't add to count, check remainder of list
+        [else (+ 1 (hamming-distance (cdr l1)(cdr l2)))] ; Add to count, check remainder of list
     )
 )
 
 (hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
+(hamming-distance (list 1 0 1 0 1 1 1 0) (list ))
